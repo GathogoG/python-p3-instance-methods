5db2a876c8635f03be3fa4eec9afe358b613f31f
George Gathogo <gathogogeorge11@gmail.com>
2024-06-02 18:40:58 +0300

    first commit

---
+++ lib/person.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+class Person:
+    # Class body goes here
+
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+    #Instance method definition
+    pass
+
+jim = Person()
+jim.talk()
+jim.walk()
